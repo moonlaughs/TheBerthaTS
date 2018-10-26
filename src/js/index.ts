c565ab11f308b1cd21c7b4aecf4e6b7bcc440c71
Megija <megi0005@edu.easj.dk>
2018-10-26 13:34:01 +0200

    meg trying to see if this shit works

--- src/js/index.ts
+++ src/js/index.ts
@@ -7,6 +7,7 @@ function greeter(person: Person): string {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
 let user: Person = { firstName: "John", lastName: "Doe" };
+let user2: Person = { firstName: "Megante", lastName: "Stepnoja"};
 
 let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
 element.innerHTML = greeter(user);
