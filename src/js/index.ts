95d7de87e3bdebab450870048c5ead8a2201900b
Diiiya <33103806+Diiiya@users.noreply.github.com>
2018-10-26 14:38:02 +0300

    Comit

--- src/js/index.ts
+++ src/js/index.ts
@@ -6,7 +6,7 @@ interface Person {
 function greeter(person: Person): string {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
-let user: Person = { firstName: "John", lastName: "Doe" };
+let user: Person = { firstName: "Diya", lastName: "Second" };
 
 let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
 element.innerHTML = greeter(user);
