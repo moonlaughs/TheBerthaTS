a501d15e5d9ddb4a141180d107beee711de7a668
Diiiya <33103806+Diiiya@users.noreply.github.com>
2018-10-28 15:08:56 +0200

    1. Changed a bit the index page just to get some values. 2. Added GetAll method in the ts files. It shows all the users. 3. Tried to add a method that gives specific user - typeerror of undefined

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,59 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import axios, {
+AxiosResponse,
+AxiosError} from "../../node_modules/axios";
+
+interface IUser {
+    id : number;
+    firstName : string;
+    lastName : string;
+    userName : string;
+    pass : string;
+    age : number;
+    gender : string;
+    typeOfUser : string;
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "Diya", lastName: "Second" };
+let output: HTMLDivElement = <HTMLDivElement> document.getElementById("output");
+let showallUsersButton: HTMLButtonElement = <HTMLButtonElement> document.getElementById("showAllUsersButton");
+showallUsersButton.addEventListener("click", showAllUsers);
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+function showAllUsers(): void {
+    let uri: string = "http://localhost:65403/api/users";
+    axios.get<IUser[]>(uri)
+    .then(function(response:AxiosResponse<IUser[]>): void {
+        let result: string = "<ol>";
+        response.data.forEach((user: IUser) => {
+            result += "<li>" + user.firstName + " " + user.lastName + "</li>"
+        });
+        result += "</ol>";
+        output.innerHTML = result;
+    })
+    .catch (function (error: AxiosError): void {
+        if (error.response) {
+            output.innerHTML = error;}
+        else {output.innerHTML = error;}
+    });
+}
+
+let showSelectedUser:HTMLButtonElement = <HTMLButtonElement> document.getElementById("showSelectedUser");
+showSelectedUser.addEventListener("click", showUser);
+
+function showUser(): void {
+    let selOutput: HTMLDivElement = <HTMLDivElement> document.getElementById("selOutput");
+    let selInput : HTMLInputElement = <HTMLInputElement> document. getElementById("selInput");
+    let id: string = selInput.value;
+    let uri: string = "http://localhost:65403/api/users/" + id;
+    axios.get<IUser>(uri)
+    .then(function (response: AxiosResponse<IUser>): void {
+        let user : IUser ;
+        response.data = user.firstName;
+        let result : string = user.firstName;
+        selOutput.innerHTML =  result;
+    })
+    .catch (function (error: AxiosError): void {
+         if (error.response) {
+             selOutput.innerHTML = error;
+         }
+         else {selOutput.innerHTML = error;}
+    });
+}
