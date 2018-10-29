c5076ce9760f2d0172bcda4a4ae2ac155e669367
Diiiya <33103806+Diiiya@users.noreply.github.com>
2018-10-29 19:04:15 +0200

    HTML: Added some forms to display the data TS: 1. The showUser() works now 2. Added method to display health data of specific user

--- src/js/index.ts
+++ src/js/index.ts
@@ -13,6 +13,16 @@ interface IUser {
     typeOfUser : string;
 }
 
+interface IHealth {
+    id : number;
+    bloodPressureUpper : number;
+    bloodPressureDown : number;
+    heartRate : number;
+    temperature : number;
+    userId : number;
+    dateTimeInfo : Date;
+}
+
 let output: HTMLDivElement = <HTMLDivElement> document.getElementById("output");
 let showallUsersButton: HTMLButtonElement = <HTMLButtonElement> document.getElementById("showAllUsersButton");
 showallUsersButton.addEventListener("click", showAllUsers);
@@ -45,9 +55,8 @@ function showUser(): void {
     let uri: string = "http://localhost:65403/api/users/" + id;
     axios.get<IUser>(uri)
     .then(function (response: AxiosResponse<IUser>): void {
-        let user : IUser ;
-        response.data = user.firstName;
-        let result : string = user.firstName;
+        console.log(response.data);
+        let result : string = response.data.firstName;
         selOutput.innerHTML =  result;
     })
     .catch (function (error: AxiosError): void {
@@ -55,5 +64,31 @@ function showUser(): void {
              selOutput.innerHTML = error;
          }
          else {selOutput.innerHTML = error;}
-    });
+    })
+}
+
+let showSelectedUsersHealthDataButton : HTMLButtonElement = <HTMLButtonElement> document.getElementById("showSelectedUsersHealthData");
+showSelectedUsersHealthDataButton.addEventListener("click", showSelUserHealthData);
+
+function showSelUserHealthData() : void {
+    let healthDataInput : HTMLInputElement = <HTMLInputElement> document.getElementById("healthDataInput");
+    let healthDataOutput : HTMLOutputElement = <HTMLOutputElement> document.getElementById("healthDataOutput");
+    let id: string = healthDataInput.value;
+    let uri: string = "http://localhost:65403/api/users/" + id + "/health";
+    axios.get<IHealth>(uri)
+    .then(function(response:AxiosResponse<IHealth[]>): void {
+        let result: string = "<table><tr><th>Id</th><th>Blood pressure upper</th><th>Blood pressure down</th><th>Hearth rate</th><th>Temperature</th><th>Date</th>" 
+                response.data.forEach((userHealthData: IHealth) => {
+            result += "<tr><td>" + userHealthData.id + "</td><td>" + userHealthData.bloodPressureUpper + "</td><td>" + userHealthData.bloodPressureDown + "</td><td>" + userHealthData.heartRate + "</td><td>" + userHealthData.temperature + "</td><td>" + userHealthData.dateTimeInfo + "</td></tr>"
+        });
+        result += "</table>"
+        healthDataOutput.innerHTML = result;
+    })
+    .catch (function (error: AxiosError): void {
+         if (error.response) {
+             healthDataOutput.innerHTML = error;}
+         else {healthDataOutput.innerHTML = error;}
+    })
+
+
 }
