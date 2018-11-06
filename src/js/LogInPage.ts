c67a30329088acd904f788f6e04bbcb4dae6bbaa
Megija <megi0005@edu.easj.dk>
2018-11-06 11:05:50 +0100

    login page update

--- src/js/LogInPage.ts
+++ src/js/LogInPage.ts
@@ -4,4 +4,24 @@ import axios,
     AxiosError
 } from "../../node_modules/axios";
 
-let usernameInput: HTMLDivElement = <HTMLDivElement>document.getElementById("username");
+interface IUser
+{
+    username: string;
+    password: string;
+}
+
+let usernameInput: HTMLInputElement = <HTMLInputElement>document.getElementById("username");
+let passwordInput: HTMLInputElement = <HTMLInputElement>document.getElementById("password");
+let loginButton: HTMLButtonElement =<HTMLButtonElement>document.getElementById("loginButton");
+loginButton.addEventListener("click", login);
+
+function login(): void
+{
+    let uri:string= "https://thebertharestconsumer20181031102055.azurewebsites.net/api/users";
+    Axios.get<IUser[]>(uri)
+    .then(function(response:AxiosResponse) :void
+    {
+        console.log(response.data);
+        let result: 
+    })
+}
